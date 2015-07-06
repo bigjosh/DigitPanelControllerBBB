f59724ce0666740c337331de19f0d66e815f2aa2
Josh Levine <github@junk.josh.com>
2015-07-05 22:51:11 -0400

    more

--- panelcontrol/www/digit57.com/cgi-bin/led.c
+++ panelcontrol/www/digit57.com/cgi-bin/led.c
@@ -1,54 +1,138 @@
 
 #include <stdio.h>
+#include <malloc.h>
 
 int main( int argc, char **argv) {
     
-    if (argc!=4) {
-        fprintf( stderr , "Sytnax: strrep count string\r\n");
+    if (argc!=5) {
+        fprintf( stderr , "Sytnax: xsize ysize stagger string\r\n");
         return(-1);
     }
     
-    int x = atoi( argv[1] );
-    int y = atoi( argv[2] );
-    char command = argv[3][0];
+    int xsize = atoi( argv[1] );
+    int ysize = atoi( argv[2] );
+	
+    int stagger = atoi( argv[3] );
+	
+	int len = xsize*ysize*3;	// 3 bytes per pixel
+	
+	printf( "%c%c" , 0x00, 0x00 );		// Channel=0 , Command=0
+	printf("%c%c" , (len>>8) , len & 0xff );  // High byte/low byte of data length
 
+    char command = argv[4][0];
+
+	char *pos = argv[4]+1;
+
+	int r,g,b;
+
+	sscanf( pos , "%2x", &r);
+	pos +=2;
+	sscanf( pos , "%2x", &g);
+	pos +=2;
+	sscanf( pos , "%2x", &b);
+	pos +=2;
+	
+	
     switch (command) {
 
-	case 'C': {
+		case 'C': {			// Straight color display
 
-		int len = x*y*3;	// 3 bytes per pixel
+	
+			int loopy=ysize;
 
-		char *pos = argv[3]+1;
+			while (loopy--) {
 
-		int r,g,b;
+				int loopx = xsize;
+				while (loopx--) {
+					   
+					   printf("%c%c%c", r ,  g, b);
 
-		sscanf( pos , "%2x", &r);
-		pos +=2;
-		sscanf( pos , "%2x", &g);
-		pos +=2;
-		sscanf( pos , "%2x", &b);
-		pos +=2;
+				}
+			}
+		}
+		break;
 
-		printf( "%c%c" , 0x00, 0x00 );		// Channel=0 , Command=0
-		printf("%c%c" , (len>>8) , len & 0xff );  // High byte/low byte of daat length
 
-		int loopy=y;
-
- 		while (loopy--) {
-
-		 int loopx = x;
-		 while (loopx--) {
-                   
-                   printf("%c%c%c", r ,  g, b);
-
-		 }
+        case 'W': {
+        
+		
+            unsigned int marginx = (xsize /4)+5 ;
+        
+        
+            int pin;
+            for(pin=0;pin<5;pin++) {
+               int string;
+               for(string=0;string<6;string++) {
+                int x;
+                for( x=0;x<xsize;x++) {
+                    if ( x>=marginx && x<(xsize-marginx) ){
+                           printf("%c%c%c", r ,  g, b);
+                        } else {
+                            printf("%c%c%c",0x00,0x00,0x00);
+                        }
+                    }
                 }
+            }
+		}
+		break;
 
-           }
-           break;
+
+		
+		case 'R': {		// Reduced 1/2 area
+		
+			unsigned int marginx = (xsize /4)+2 ;
+			unsigned int marginy = ysize /4 ;
+            
+            unsigned long *buffer=malloc( 97 * 7 * 3 );
+
+            unsigned long rgb= ((unsigned long) r << 16) | (g << 8) | b ;
+            
+            
+            int y;
+            for( y=0;y<ysize;y++) {
+                
+                int x;
+                for( x=0;x<xsize;x++) {
+
+					if ( ( y >= marginy && y < (y-marginy) ) && ( x>=marginx && x<(x-marginx) ) ){
+					
+                       buffer[ x + (y*ysize)] = rgb; 
+					   printf("%c%c%c", r ,  g, b);
+					
+                    } else {
+					
+                        buffer[ x + (y*ysize)] = 0x00;
+						//Otherwise send black in the margin
+					}
 
 
-    }
+                }
+            }
+            
+            // now send it.
+            // row #4 first, then row 9 (4+5 spacing), then row 14... then row #3, row #8...
+		
+            int pin;
+            for( pin = 0; pin<5; pin++ ) {
+            
+                int string;
+                for ( string=0; string<5 ; string++ ) {
+
+                    y = (4-pin) * (5*string);;
+                    
+                    int x;
+                    for( x=0; x<97; x++ ) {
+                    
+                        printf( "%.6lx" , buffer[x+(y*ysize)] );
+                    
+                    }
+                
+                }
+            }
+        }
+      
+	}
+	
     return(0);
     
 }
--- panelcontrol/www/digit57.com/cgi-bin/light.cgi
+++ panelcontrol/www/digit57.com/cgi-bin/light.cgi
@@ -2,7 +2,7 @@
 echo "Content-type: text/html"
 echo ''
 echo 'CGI Bash example<br>'
-./a.out 5 582 $QUERY_STRING >/dev/tcp/localhost/7890
+./a.out 57 30 5 $QUERY_STRING >/dev/tcp/localhost/7890
 echo $QUERY_STRING
 echo "Blue! " 
 
